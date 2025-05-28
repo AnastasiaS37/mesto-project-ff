a90081065e5c5c19c17c496e61b5522a8c4ef2ec
AnastasiaS <anastasiagir@gmail.com>
2025-05-28 21:48:15 +0300

    Final version

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
-// @todo: Темплейт карточки
+function createCard(cardElement, deleteCardFunction) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  // клонируем содержимое тега template и наполняем данными
+  const newCardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  newCardElement.querySelector('.card__image').src = cardElement.link;
+  newCardElement.querySelector('.card__title').textContent = cardElement.name;
 
-// @todo: DOM узлы
+  const deleteButton = newCardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', function (evt) {
+    const cardToDelete = deleteButton.closest('.card');
+    deleteCardFunction(cardToDelete);
+  });
 
-// @todo: Функция создания карточки
+  return newCardElement;
+}
 
-// @todo: Функция удаления карточки
+function deleteCard(cardToDelete) {
+  cardToDelete.remove();
+  return;
+};
 
-// @todo: Вывести карточки на страницу
+const places = document.querySelector('.places__list');
+initialCards.forEach(function (item) {
+  const cardToAdd = createCard(item, deleteCard);
+  places.append(cardToAdd);
+});
